cf1fd4dbc441013b9ec7df99f3f75334e8726551
Andrey Lemin <anlemin@mail.ru>
2021-10-01 01:18:19 +0300

    ImproveLayoutTableView

--- Cyberpunk2077-hack-helper.LayoutMarker/LayoutTableView.cs
+++ Cyberpunk2077-hack-helper.LayoutMarker/LayoutTableView.cs
@@ -1,5 +1,4 @@
-﻿using Cyberpunk2077_hack_helper.Grabbing;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Media;
 
@@ -7,26 +6,42 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 {
 	public class LayoutTableView : FrameworkElement
 	{
-		public static readonly DependencyProperty LayoutTableProperty;
+		public static readonly DependencyProperty PositionProperty;
+		public static readonly DependencyProperty CellSizeProperty;
+		public static readonly DependencyProperty CellCountProperty;
 		public static readonly DependencyProperty BrushProperty;
-
-		private Brush _brush;
-		private Pen _pen;
-
-		// Create a collection of child visual objects.
-		private readonly VisualCollection _visuals;
 
 		static LayoutTableView()
 		{
-			LayoutTableProperty = DependencyProperty.Register(
-						"LayoutTable",
-						typeof(LayoutTableViewModel),
+			PositionProperty = DependencyProperty.Register(
+						"Position",
+						typeof(System.Drawing.Point),
 						typeof(LayoutTableView),
 						new FrameworkPropertyMetadata(
-							null,
+							new System.Drawing.Point(0, 0),
 							FrameworkPropertyMetadataOptions.AffectsMeasure |
 							FrameworkPropertyMetadataOptions.AffectsRender,
-							new PropertyChangedCallback(OnLayoutTableChanged)));
+							new PropertyChangedCallback(OnPositionChanged)));
+
+			CellSizeProperty = DependencyProperty.Register(
+						"CellSize",
+						typeof(System.Drawing.Size),
+						typeof(LayoutTableView),
+						new FrameworkPropertyMetadata(
+							new System.Drawing.Size(0, 0),
+							FrameworkPropertyMetadataOptions.AffectsMeasure |
+							FrameworkPropertyMetadataOptions.AffectsRender,
+							new PropertyChangedCallback(OnCellSizeChanged)));
+
+			CellCountProperty = DependencyProperty.Register(
+						"CellCount",
+						typeof(System.Drawing.Size),
+						typeof(LayoutTableView),
+						new FrameworkPropertyMetadata(
+							new System.Drawing.Size(0, 0),
+							FrameworkPropertyMetadataOptions.AffectsMeasure |
+							FrameworkPropertyMetadataOptions.AffectsRender,
+							new PropertyChangedCallback(OnCellCountChanged)));
 
 			BrushProperty = DependencyProperty.Register(
 						"Brush",
@@ -38,10 +53,32 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 							new PropertyChangedCallback(OnBrushChanged)));
 		}
 
-		public LayoutTableViewModel LayoutTable
+		private System.Drawing.Point _position;
+		private System.Drawing.Size _cellSize;
+		private System.Drawing.Size _cellCount;
+
+		private Brush _brush;
+		private Pen _pen;
+
+		// Create a collection of child visual objects.
+		private readonly VisualCollection _visuals;
+
+		public System.Drawing.Point Position
 		{
-			get { return (LayoutTableViewModel)GetValue(LayoutTableProperty); }
-			set { SetValue(LayoutTableProperty, value); }
+			get { return (System.Drawing.Point)GetValue(PositionProperty); }
+			set { SetValue(PositionProperty, value); }
+		}
+
+		public System.Drawing.Size CellSize
+		{
+			get { return (System.Drawing.Size)GetValue(CellSizeProperty); }
+			set { SetValue(CellSizeProperty, value); }
+		}
+
+		public System.Drawing.Size CellCount
+		{
+			get { return (System.Drawing.Size)GetValue(CellCountProperty); }
+			set { SetValue(CellCountProperty, value); }
 		}
 
 		public Brush Brush
@@ -61,43 +98,12 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 			};
 		}
 
-		// Create a DrawingVisual that contains a rectangle.
-		private void DrawVisualLayout(DrawingVisual drawingVisual, LayoutTable layoutTable)
-		{
-			// Retrieve the DrawingContext in order to create new drawing content.
-			using (DrawingContext drawingContext = drawingVisual.RenderOpen())
-			{
-				for (int row = 0; row <= layoutTable.CellCount.Height; ++row)
-				{
-					drawingContext.DrawLine(
-						_pen,
-						new Point(
-							layoutTable.Position.X,
-							layoutTable.Position.Y + row * layoutTable.CellSize.Height),
-						new Point(
-							layoutTable.Position.X + layoutTable.CellCount.Width * layoutTable.CellSize.Width,
-							layoutTable.Position.Y + row * layoutTable.CellSize.Height));
-				}
-
-				for (int col = 0; col <= layoutTable.CellCount.Width; ++col)
-				{
-					drawingContext.DrawLine(
-						_pen,
-						new Point(
-							layoutTable.Position.X + col * layoutTable.CellSize.Width,
-							layoutTable.Position.Y),
-						new Point(
-							layoutTable.Position.X + col * layoutTable.CellSize.Width,
-							layoutTable.Position.Y + layoutTable.CellCount.Height * layoutTable.CellSize.Height));
-				}
-
-				// Persist the drawing content.
-				drawingContext.Close();
-			}
-		}
-
-		// Provide a required override for the VisualChildrenCount property.
 		protected override int VisualChildrenCount => _visuals.Count;
+
+		private void RedrawSelf()
+		{
+			RedrawGrid((DrawingVisual)GetVisualChild(0), _pen, _position, _cellSize, _cellCount);
+		}
 
 		// Provide a required override for the GetVisualChild method.
 		protected override Visual GetVisualChild(int index)
@@ -110,12 +116,31 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 			return _visuals[index];
 		}
 
-		private static void OnLayoutTableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		private static void OnPositionChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 		{
 			LayoutTableView thisObj = (LayoutTableView)d;
-			LayoutTableViewModel value = (LayoutTableViewModel)e.NewValue;
+			System.Drawing.Point position = (System.Drawing.Point)e.NewValue;
 
-			thisObj.DrawVisualLayout((DrawingVisual)thisObj.GetVisualChild(0), value.Model);
+			thisObj._position = position;
+			thisObj.RedrawSelf();
+		}
+
+		private static void OnCellSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			LayoutTableView thisObj = (LayoutTableView)d;
+			System.Drawing.Size cellSize = (System.Drawing.Size)e.NewValue;
+
+			thisObj._cellSize = cellSize;
+			thisObj.RedrawSelf();
+		}
+
+		private static void OnCellCountChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			LayoutTableView thisObj = (LayoutTableView)d;
+			System.Drawing.Size cellCount = (System.Drawing.Size)e.NewValue;
+
+			thisObj._cellCount = cellCount;
+			thisObj.RedrawSelf();
 		}
 
 		private static void OnBrushChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -125,6 +150,41 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 
 			thisObj._brush = brush;
 			thisObj._pen = new Pen(brush, 1.0);
+			thisObj.RedrawSelf();
+		}
+
+		private static void RedrawGrid(DrawingVisual drawingVisual, Pen pen, System.Drawing.Point position, System.Drawing.Size cellSize, System.Drawing.Size cellCount)
+		{
+			// Retrieve the DrawingContext in order to create new drawing content.
+			using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+			{
+				for (int row = 0; row <= cellCount.Height; ++row)
+				{
+					drawingContext.DrawLine(
+						pen,
+						new Point(
+							position.X,
+							position.Y + row * cellSize.Height),
+						new Point(
+							position.X + cellCount.Width * cellSize.Width,
+							position.Y + row * cellSize.Height));
+				}
+
+				for (int col = 0; col <= cellCount.Width; ++col)
+				{
+					drawingContext.DrawLine(
+						pen,
+						new Point(
+							position.X + col * cellSize.Width,
+							position.Y),
+						new Point(
+							position.X + col * cellSize.Width,
+							position.Y + cellCount.Height * cellSize.Height));
+				}
+
+				// Persist the drawing content.
+				drawingContext.Close();
+			}
 		}
 	}
 }
