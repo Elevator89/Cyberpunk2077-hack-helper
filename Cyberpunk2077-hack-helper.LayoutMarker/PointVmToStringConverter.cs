3f66c486e10f78812d4a381ce99ecfc2df21fbee
Andrey Lemin <anlemin@mail.ru>
2021-10-07 00:38:17 +0300

    Fix PointViewModel and add PointVmToStringConverter (squash)

---
+++ Cyberpunk2077-hack-helper.LayoutMarker/PointVmToStringConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Windows.Data;
+using System;
+using System.Globalization;
+
+namespace Cyberpunk2077_hack_helper.LayoutMarker
+{
+	public class PointVmToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			PointViewModel pointViewModel = (PointViewModel)value;
+			return $"{pointViewModel.X}; {pointViewModel.Y}";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string pointStr = (string)value;
+			string[] coords = pointStr.Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+			Point point = new Point(int.Parse(coords[0]), int.Parse(coords[1]));
+			return new PointViewModel(point);
+		}
+	}
+}
