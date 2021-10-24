abadd8055a75bb6f691b9a737b98d0ac7ab14ca2
Andrey Lemin <anlemin@mail.ru>
2021-10-25 00:31:07 +0300

    Implement Save and Open

--- Cyberpunk2077-hack-helper.LayoutMarker/ApplicationViewModel.cs
+++ Cyberpunk2077-hack-helper.LayoutMarker/ApplicationViewModel.cs
@@ -4,11 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace Cyberpunk2077_hack_helper.LayoutMarker
 {
@@ -31,23 +27,68 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 			_dialogService = dialogService;
 			_fileService = fileService;
 
-			_model = new Layout(
+			_model = CreateDefaultLayout();
+			_layoutViewModel = new LayoutViewModel(_model);
+		}
+
+		public void New()
+		{
+			_model = CreateDefaultLayout();
+			_layoutViewModel.Model = _model;
+		}
+
+		public void Open()
+		{
+			try
+			{
+				if (_dialogService.OpenFileDialog() == true)
+				{
+					_model = _fileService.Open(_dialogService.FilePath);
+					Layout.Model = _model;
+					_dialogService.ShowMessage("Файл открыт");
+				}
+			}
+			catch (Exception ex)
+			{
+				_dialogService.ShowMessage(ex.Message);
+			}
+		}
+
+		public void Save()
+		{
+			try
+			{
+				if (_dialogService.SaveFileDialog() == true)
+				{
+					_fileService.Save(_dialogService.FilePath, _model);
+					_dialogService.ShowMessage("Файл сохранён");
+				}
+			}
+			catch (Exception ex)
+			{
+				_dialogService.ShowMessage(ex.Message);
+			}
+		}
+
+		private Layout CreateDefaultLayout()
+		{
+			return new Layout(
 				matrix: new LayoutTable()
 				{
 					Position = new Point(100, 100),
 					CellSize = new Size(10, 10),
 					CellCount = new Size(6, 6),
 					SymbolMaps = new List<SymbolMap>() {
-						new SymbolMap(Symbol._1C, new List<Point>(){
-							new Point(1,1),
-							new Point(2,2),
-							new Point(3,3),
-						}),
-						new SymbolMap(Symbol._55, new List<Point>(){
-							new Point(1,5),
-							new Point(2,5),
-							new Point(3,5),
-						})
+											new SymbolMap(Symbol._1C, new List<Point>(){
+												new Point(1,1),
+												new Point(2,2),
+												new Point(3,3),
+											}),
+											new SymbolMap(Symbol._55, new List<Point>(){
+												new Point(1,5),
+												new Point(2,5),
+												new Point(3,5),
+											})
 					}
 				},
 				sequences: new LayoutTable()
@@ -56,74 +97,19 @@ namespace Cyberpunk2077_hack_helper.LayoutMarker
 					CellSize = new Size(20, 20),
 					CellCount = new Size(8, 2),
 					SymbolMaps = new List<SymbolMap>() {
-						new SymbolMap(Symbol._7A, new List<Point>(){
-							new Point(7,1),
-							new Point(7,2),
-							new Point(7,3),
-						}),
-						new SymbolMap(Symbol._BD, new List<Point>(){
-							new Point(8,5),
-							new Point(8,5),
-							new Point(8,5),
-						})
+											new SymbolMap(Symbol._7A, new List<Point>(){
+												new Point(7,1),
+												new Point(7,2),
+												new Point(7,3),
+											}),
+											new SymbolMap(Symbol._BD, new List<Point>(){
+												new Point(8,5),
+												new Point(8,5),
+												new Point(8,5),
+											})
 					}
 				});
-
-			_layoutViewModel = new LayoutViewModel(_model);
 		}
-
-		//// команда сохранения файла
-		//private RelayCommand saveCommand;
-		//public RelayCommand SaveCommand
-		//{
-		//	get
-		//	{
-		//		return saveCommand ??
-		//		  (saveCommand = new RelayCommand(obj =>
-		//		  {
-		//			  try
-		//			  {
-		//				  if (dialogService.SaveFileDialog() == true)
-		//				  {
-		//					  fileService.Save(dialogService.FilePath, Phones.ToList());
-		//					  dialogService.ShowMessage("Файл сохранен");
-		//				  }
-		//			  }
-		//			  catch (Exception ex)
-		//			  {
-		//				  dialogService.ShowMessage(ex.Message);
-		//			  }
-		//		  }));
-		//	}
-		//}
-
-		//// команда открытия файла
-		//private RelayCommand openCommand;
-		//public RelayCommand OpenCommand
-		//{
-		//	get
-		//	{
-		//		return openCommand ??
-		//		  (openCommand = new RelayCommand(obj =>
-		//		  {
-		//			  try
-		//			  {
-		//				  if (dialogService.OpenFileDialog() == true)
-		//				  {
-		//					  var phones = fileService.Open(dialogService.FilePath);
-		//					  Phones.Clear();
-		//					  foreach (var p in phones)
-		//						  Phones.Add(p);
-		//					  dialogService.ShowMessage("Файл открыт");
-		//				  }
-		//			  }
-		//			  catch (Exception ex)
-		//			  {
-		//				  dialogService.ShowMessage(ex.Message);
-		//			  }
-		//		  }));
-		//	}
-		//}
 
 		private void OnPropertyChanged([CallerMemberName] string prop = "")
 		{
