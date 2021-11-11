00cf5f5cbcdeebfeb8d77b94162bf0121aa0b030
Andrey Lemin <anlemin@mail.ru>
2021-11-12 01:12:37 +0300

    Add TargetId to Drag

--- Cyberpunk2077-hack-helper.LayoutMarker/Views/Drag.cs
+++ Cyberpunk2077-hack-helper.LayoutMarker/Views/Drag.cs
@@ -2,16 +2,19 @@
 
 namespace Cyberpunk2077_hack_helper.LayoutMarker.Views
 {
-	public class Drag
+	public class Drag<TTargetId>
 	{
+		public TTargetId TargetId { get; }
+
 		public Point Start { get; }
 		public System.Drawing.Point TargetStart { get; }
 		public Vector Vector { get { return End - Start; } }
 		public Point End { get; private set; }
 		public System.Drawing.Point TargetEnd { get; private set; }
 
-		public Drag(Point start, System.Drawing.Point targetStart)
+		public Drag(TTargetId id, Point start, System.Drawing.Point targetStart)
 		{
+			TargetId = id;
 			Start = start;
 			TargetStart = targetStart;
 		}
