43b144daf7c1d26c4fdfb7356fb283018843bdfc
Andrey Lemin <anlemin@mail.ru>
2021-11-20 18:01:35 +0300

    Add stub test project and test screenshots

---
+++ Cyberpunk2077-hack-helper.Grabbing.Test/GrabbingTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Cyberpunk2077_hack_helper.Grabbing.Test
+{
+	[TestClass]
+	public class GrabbingTests
+	{
+		[TestMethod]
+		public void Grabs7x7()
+		{
+
+
+		}
+	}
+}
