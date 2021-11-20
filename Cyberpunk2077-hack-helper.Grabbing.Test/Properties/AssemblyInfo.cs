7ae030bd7c18c5c53e86183df33d15337e451810
Andrey Lemin <anlemin@mail.ru>
2021-11-20 18:01:35 +0300

    Add stub test project and test screenshots

---
+++ Cyberpunk2077-hack-helper.Grabbing.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cyberpunk2077-hack-helper.Grabbing.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cyberpunk2077-hack-helper.Grabbing.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7bf86d65-6ae3-4e7d-a70f-7e850db0a2a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
