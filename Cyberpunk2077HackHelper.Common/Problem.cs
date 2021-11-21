1a5810d14e83ed21d370007d8e8a8abfa87a7a8e
Andrey Lemin <anlemin@mail.ru>
2021-11-22 00:18:26 +0300

    Read Problem from int arrays

--- Cyberpunk2077HackHelper.Common/Problem.cs
+++ Cyberpunk2077HackHelper.Common/Problem.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Cyberpunk2077HackHelper.Common
 {
@@ -13,6 +14,24 @@ namespace Cyberpunk2077HackHelper.Common
 			Matrix = matrix;
 			DaemonSequences = daemonSequences;
 			BufferLength = bufferLength;
+		}
+
+		public static Problem FromHex(int[,] matrixHex, IReadOnlyList<IReadOnlyList<int>> daemonSequencesHex, int bufferLength)
+		{
+			Symbol[,] matrix = new Symbol[matrixHex.GetLength(0), matrixHex.GetLength(1)];
+			for (int row = 0; row < matrixHex.GetLength(0); ++row)
+				for (int col = 0; col < matrixHex.GetLength(1); ++col)
+				{
+					matrix[row, col] = (Symbol)matrixHex[row, col];
+				}
+
+			List<IReadOnlyList<Symbol>> daemonSequences = new List<IReadOnlyList<Symbol>>(daemonSequencesHex.Count);
+			foreach (IReadOnlyList<int> sequenceHex in daemonSequencesHex)
+			{
+				daemonSequences.Add(sequenceHex.Select(n => (Symbol)n).ToArray());
+			}
+
+			return new Problem(matrix, daemonSequences, bufferLength);
 		}
 	}
 }
