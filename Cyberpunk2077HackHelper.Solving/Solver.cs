290bec80e968bda20ff09620e2122e079a59dfbb
Andrey Lemin <anlemin@mail.ru>
2021-11-27 00:25:28 +0300

    Add Solver

---
+++ Cyberpunk2077HackHelper.Solving/Solver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using Cyberpunk2077HackHelper.Common;
+
+namespace Cyberpunk2077HackHelper.Solving
+{
+	public class Solver
+	{
+		public Point[] Solve(Problem problem)
+		{
+
+		}
+	}
+}
