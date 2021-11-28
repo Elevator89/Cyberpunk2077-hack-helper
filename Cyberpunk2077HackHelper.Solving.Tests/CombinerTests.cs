c798ea6b8b3d50f808db3ce84ecc3d2b41f76b82
Andrey Lemin <anlemin@mail.ru>
2021-11-29 00:45:26 +0300

    Add tests for combinations

--- Cyberpunk2077HackHelper.Solving.Tests/CombinerTests.cs
+++ Cyberpunk2077HackHelper.Solving.Tests/CombinerTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +8,14 @@ namespace Cyberpunk2077HackHelper.Solving.Tests
 	[TestClass]
 	public class CombinerTests
 	{
+		private Combiner<int> _combiner;
+
+		[TestInitialize]
+		public void Init()
+		{
+			_combiner = new Combiner<int>(EqualityComparer<int>.Default);
+		}
+
 		[DataTestMethod]
 		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 }, 0, 0)]
 		[DataRow(new[] { 1, 1, 2, 3, 4 }, new[] { 1, 2 }, 0, 1)]
@@ -15,7 +24,7 @@ namespace Cyberpunk2077HackHelper.Solving.Tests
 		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 3, 4, 5, 6 }, 0, 2)]
 		public void FindsSubsequence(int[] seqA, int[] seqB, int startFrom, int expectedIntersectionStart)
 		{
-			int actualIntersectionStart = Combiner.GetIntersectionStart(seqA, seqB, startFrom, (a, b) => a == b);
+			int actualIntersectionStart = _combiner.GetIntersectionStart(seqA, seqB, startFrom);
 			Assert.AreEqual(expectedIntersectionStart, actualIntersectionStart);
 		}
 
@@ -31,8 +40,116 @@ namespace Cyberpunk2077HackHelper.Solving.Tests
 		[DataRow(new[] { 0, 1, 1, 2, 1, 2, 1, 3 }, new[] { 1, 2 }, 5, new[] { 0, 1, 1, 2, 1, 2, 1, 3, 1, 2 })]
 		public void GetsCombination(int[] seqA, int[] seqB, int startFrom, int[] expSeq)
 		{
-			int[] actualSeq = Combiner.GetCombination(seqA, seqB, startFrom, (a, b) => a == b, out int intersectionStart).ToArray();
+			int[] actualSeq = _combiner.GetCombination(seqA, seqB, startFrom, out int intersectionStart).ToArray();
 			CollectionAssert.AreEqual(expSeq, actualSeq);
+		}
+
+		[DataTestMethod]
+		[DataRow(0)]
+		[DataRow(1)]
+		[DataRow(2)]
+		public void GetPossibleCombinations(int caseId)
+		{
+			(IReadOnlyList<int> seqA, IReadOnlyList<int> seqB, int wildValue, int wildMaxCount, IReadOnlyList<int>[] expectedCombinations) = GetSequencesAndCombinations(caseId);
+
+			IReadOnlyList<int>[] combinations = _combiner.GetPossibleCombinations(seqA, seqB, wildValue, wildMaxCount).ToArray();
+			for (int i = 0; i < combinations.Length; ++i)
+			{
+				CollectionAssert.AreEqual(expectedCombinations[i].ToArray(), combinations[i].ToArray());
+			}
+		}
+
+		[DataTestMethod]
+		[DataRow(0)]
+		[DataRow(1)]
+		public void GetAllPossibleCombinations(int caseId)
+		{
+			(IReadOnlyList<int>[] sequences, int wildValue, int wildMaxCount, IReadOnlyList<int>[] expectedCombinations) = GetAllSequencesAndCombinations(caseId);
+
+			IReadOnlyList<int>[] combinations = _combiner.GetPossibleCombinations(sequences, wildValue, wildMaxCount).ToArray();
+			for (int i = 0; i < combinations.Length; ++i)
+			{
+				CollectionAssert.AreEqual(expectedCombinations[i].ToArray(), combinations[i].ToArray());
+			}
+		}
+
+		private (IReadOnlyList<int> seqA, IReadOnlyList<int> seqB, int wildValue, int wildMaxCount, IReadOnlyList<int>[] expectedCombinations) GetSequencesAndCombinations(int caseId)
+		{
+			switch (caseId)
+			{
+				case 0:
+					return (
+						new[] { 1, 2 },
+						new[] { 1, 2 },
+						-1, 0,
+						new[] {
+							new[] { 1, 2 },
+							new[] { 1, 2, 1, 2 }
+						});
+				case 1:
+					return (
+						new[] { 1, 2 },
+						new[] { 1, 2 },
+						-1, 1,
+						new[] {
+							new[] { 1, 2 },
+							new[] { 1, 2, 1, 2 },
+							new[] { 1, 2, -1, 1, 2 },
+						});
+				case 2:
+					return (
+						new[] { 1, 2 },
+						new[] { 1, 2 },
+						-1, 2,
+						new[] {
+							new[] { 1, 2 },
+							new[] { 1, 2, 1, 2 },
+							new[] { 1, 2, -1, 1, 2 },
+							new[] { 1, 2, -1, -1, 1, 2 },
+						});
+				default:
+					throw new NotImplementedException();
+			}
+		}
+
+		private (IReadOnlyList<int>[] sequences, int wildValue, int wildMaxCount, IReadOnlyList<int>[] expectedCombinations) GetAllSequencesAndCombinations(int caseId)
+		{
+			switch (caseId)
+			{
+				case 0:
+					return (
+						new[] {
+							new[] { 1, 2 },
+							new[] { 1, 2 },
+							new[] { 1, 2 },
+						},
+						-1, 0,
+						new[] {
+							new[] { 1, 2 },
+							new[] { 1, 2, 1, 2 },
+							new[] { 1, 2, 1, 2, 1, 2 },
+						});
+				case 1:
+					return (
+						new[] {
+							new[] { 1, 2 },
+							new[] { 3, 4 },
+							new[] { 5, 6 },
+						},
+						-1, 1,
+						new[] {
+							new[] { 1, 2, 3, 4, 5, 6 },
+							new[] { -1, 1, 2, 3, 4, 5, 6 },
+							new[] { 1, 2, 3, 4, -1, 5, 6 },
+							new[] { -1, 1, 2, 3, 4, -1, 5, 6 },
+							new[] { 1, 2, -1, 3, 4, 5, 6 },
+							new[] { -1, 1, 2, -1, 3, 4, 5, 6 },
+							new[] { 1, 2, -1, 3, 4, -1, 5, 6 },
+							new[] { -1, 1, 2, -1, 3, 4, -1, 5, 6 },
+						});
+				default:
+					throw new NotImplementedException();
+			}
 		}
 	}
 }
--- Cyberpunk2077HackHelper.Solving/Combiner.cs
+++ Cyberpunk2077HackHelper.Solving/Combiner.cs
@@ -1,66 +1,119 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Cyberpunk2077HackHelper.Solving
 {
 	public delegate bool Equals<T>(T a, T b);
 
-	public class Combiner
+	public class Combiner<T>
 	{
-		public static IEnumerable<IReadOnlyList<T>> GetPossibleCombinations<T>(IReadOnlyList<IReadOnlyList<T>> sequences, Equals<T> equals, T wildValue, int wildMaxCount)
+		private class ListComparer : IEqualityComparer<IReadOnlyList<T>>
 		{
-			foreach (IReadOnlyList<T> fullCombination in GetPossibleCombinationsRecursively(sequences[0], sequences, 1, equals, wildValue, wildMaxCount))
+			private IEqualityComparer<T> _comparer;
+
+			public ListComparer(IEqualityComparer<T> comparer)
 			{
-				yield return fullCombination;
+				_comparer = comparer;
+			}
+
+			public bool Equals(IReadOnlyList<T> x, IReadOnlyList<T> y)
+			{
+				if (x.Count != y.Count)
+					return false;
+
+				for (int i = 0; i < x.Count; ++i)
+				{
+					if (!_comparer.Equals(x[i], y[i]))
+						return false;
+				}
+				return true;
+			}
+
+			public int GetHashCode(IReadOnlyList<T> obj)
+			{
+				int hash = 0;
+				for (int i = 0; i < obj.Count; ++i)
+				{
+					hash = ShiftAndWrap(hash, 2) ^ _comparer.GetHashCode(obj[i]);
+				}
+				return hash;
+			}
+
+			private static int ShiftAndWrap(int value, int positions)
+			{
+				positions = positions & 0x1F;
+
+				// Save the existing bit pattern, but interpret it as an unsigned integer.
+				uint number = BitConverter.ToUInt32(BitConverter.GetBytes(value), 0);
+				// Preserve the bits to be discarded.
+				uint wrapped = number >> (32 - positions);
+				// Shift and wrap the discarded bits.
+				return BitConverter.ToInt32(BitConverter.GetBytes((number << positions) | wrapped), 0);
 			}
 		}
 
-		private static IEnumerable<IReadOnlyList<T>> GetPossibleCombinationsRecursively<T>(IReadOnlyList<T> currentCombination, IReadOnlyList<IReadOnlyList<T>> sequences, int startFrom, Equals<T> equals, T wildValue, int wildMaxCount)
+		private readonly IEqualityComparer<T> _comparer;
+		private readonly ListComparer _listComparer;
+
+		public Combiner(IEqualityComparer<T> comparer)
+		{
+			_comparer = comparer;
+			_listComparer = new ListComparer(_comparer);
+		}
+
+		public IEnumerable<IReadOnlyList<T>> GetPossibleCombinations(IReadOnlyList<IReadOnlyList<T>> sequences, T wildValue, int wildMaxCount)
+		{
+			foreach (IReadOnlyList<T> fullCombination in GetPossibleCombinationsRecursively(sequences[0], sequences, 1, wildValue, wildMaxCount).Distinct(_listComparer))
+			{
+				for (int wildCount = 0; wildCount < wildMaxCount; ++wildCount)
+					yield return Enumerable.Repeat(wildValue, wildCount).Concat(fullCombination).ToArray();
+			}
+		}
+
+		private IEnumerable<IReadOnlyList<T>> GetPossibleCombinationsRecursively(IReadOnlyList<T> currentCombination, IReadOnlyList<IReadOnlyList<T>> sequences, int startFrom, T wildValue, int wildMaxCount)
 		{
 			if (startFrom == sequences.Count)
 				yield return currentCombination;
 			else
-				foreach (IReadOnlyList<T> newCombination in GetPossibleCombinations(currentCombination, sequences[startFrom], equals, wildValue, wildMaxCount))
+				foreach (IReadOnlyList<T> newCombination in GetPossibleCombinations(currentCombination, sequences[startFrom], wildValue, wildMaxCount).Distinct(_listComparer))
 				{
-					foreach (IReadOnlyList<T> fullCombination in GetPossibleCombinationsRecursively(newCombination, sequences, startFrom + 1, equals, wildValue, wildMaxCount))
+					foreach (IReadOnlyList<T> fullCombination in GetPossibleCombinationsRecursively(newCombination, sequences, startFrom + 1, wildValue, wildMaxCount).Distinct(_listComparer))
 						yield return fullCombination;
 				}
 		}
 
-		public static IEnumerable<IReadOnlyList<T>> GetPossibleCombinations<T>(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, Equals<T> equals, T wildValue, int wildMaxCount)
+		public IEnumerable<IReadOnlyList<T>> GetPossibleCombinations(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, T wildValue, int wildMaxCount)
 		{
 			int intersectionStart = 0;
 			do
 			{
-				yield return GetCombination(seqA, seqB, intersectionStart, equals, out intersectionStart);
+				yield return GetCombination(seqA, seqB, intersectionStart, out intersectionStart);
 				intersectionStart++;
 			}
 			while (intersectionStart <= seqA.Count);
 
-			for (int wildCount = 0; wildCount < wildMaxCount; ++wildCount)
+			for (int wildCount = 1; wildCount < wildMaxCount; ++wildCount)
 				yield return seqA.Concat(Enumerable.Repeat(wildValue, wildCount)).Concat(seqB).ToArray();
 		}
 
-		public static IReadOnlyList<T> GetCombination<T>(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, int startFrom, Equals<T> equals, out int intersectionStart)
+		public IReadOnlyList<T> GetCombination(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, int startFrom, out int intersectionStart)
 		{
-			intersectionStart = GetIntersectionStart(seqA, seqB, startFrom, equals);
+			intersectionStart = GetIntersectionStart(seqA, seqB, startFrom);
 			if (seqA.Count - intersectionStart < seqB.Count)
 				return seqA.Take(intersectionStart).Concat(seqB).ToArray();
 			return seqA;
 		}
 
-		public static int GetIntersectionStart<T>(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, int startFrom, Equals<T> equals)
+		public int GetIntersectionStart(IReadOnlyList<T> seqA, IReadOnlyList<T> seqB, int startFrom)
 		{
-			// A: 1 1 1 2 3 4
-			// B: 1 1 2 3  
-
 			int indA = startFrom;
 			int indB = 0;
 			int intersectionStart = startFrom;
 
 			while (indA < seqA.Count && indB < seqB.Count)
 			{
-				if (equals(seqA[indA], seqB[indB]))
+				if (_comparer.Equals(seqA[indA], seqB[indB]))
 				{
 					indA++;
 					indB++;
