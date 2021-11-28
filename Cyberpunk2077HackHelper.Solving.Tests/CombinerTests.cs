01a654d49740a983dddd090a0f0610ec97baacb8
Andrey Lemin <anlemin@mail.ru>
2021-11-28 22:41:39 +0300

    Add Solving classes with some tests

---
+++ Cyberpunk2077HackHelper.Solving.Tests/CombinerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cyberpunk2077HackHelper.Solving.Tests
+{
+	[TestClass]
+	public class CombinerTests
+	{
+		[DataTestMethod]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 }, 0, 0)]
+		[DataRow(new[] { 1, 1, 2, 3, 4 }, new[] { 1, 2 }, 0, 1)]
+		[DataRow(new[] { 1, 1, 2, 1, 2 }, new[] { 1, 2 }, 2, 3)]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 5, 6 }, 0, 4)]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 3, 4, 5, 6 }, 0, 2)]
+		public void FindsSubsequence(int[] seqA, int[] seqB, int startFrom, int expectedIntersectionStart)
+		{
+			int actualIntersectionStart = Combiner.GetIntersectionStart(seqA, seqB, startFrom, (a, b) => a == b);
+			Assert.AreEqual(expectedIntersectionStart, actualIntersectionStart);
+		}
+
+		[DataTestMethod]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 3, 4 }, 0, new[] { 1, 2, 3, 4 })]
+		[DataRow(new[] { 1, 1, 2, 3, 4 }, new[] { 1, 2 }, 0, new[] { 1, 1, 2, 3, 4 })]
+		[DataRow(new[] { 1, 1, 2, 1, 2 }, new[] { 1, 2 }, 2, new[] { 1, 1, 2, 1, 2 })]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 2, 5, 6 }, 0, new[] { 1, 2, 3, 4, 1, 2, 5, 6 })]
+		[DataRow(new[] { 1, 2, 3, 4 }, new[] { 3, 4, 5, 6 }, 0, new[] { 1, 2, 3, 4, 5, 6 })]
+		[DataRow(new[] { 1, 2 }, new[] { 1, 2, 3, 4 }, 0, new[] { 1, 2, 3, 4 })]
+		[DataRow(new[] { 0, 1, 1, 2, 1, 2, 1, 3 }, new[] { 1, 2 }, 0, new[] { 0, 1, 1, 2, 1, 2, 1, 3 })]
+		[DataRow(new[] { 0, 1, 1, 2, 1, 2, 1, 3 }, new[] { 1, 2 }, 3, new[] { 0, 1, 1, 2, 1, 2, 1, 3 })]
+		[DataRow(new[] { 0, 1, 1, 2, 1, 2, 1, 3 }, new[] { 1, 2 }, 5, new[] { 0, 1, 1, 2, 1, 2, 1, 3, 1, 2 })]
+		public void GetsCombination(int[] seqA, int[] seqB, int startFrom, int[] expSeq)
+		{
+			int[] actualSeq = Combiner.GetCombination(seqA, seqB, startFrom, (a, b) => a == b, out int intersectionStart).ToArray();
+			CollectionAssert.AreEqual(expSeq, actualSeq);
+		}
+	}
+}
---
+++ Cyberpunk2077HackHelper.Solving.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cyberpunk2077HackHelper.Solving.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cyberpunk2077HackHelper.Solving.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fb0f9719-848d-444d-bc57-67a222615020")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
