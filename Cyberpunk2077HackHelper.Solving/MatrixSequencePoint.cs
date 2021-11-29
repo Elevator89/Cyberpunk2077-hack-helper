95711fad1aa19b03bfd40c71b80d40bc6a72dca8
Andrey Lemin <anlemin@mail.ru>
2021-11-30 02:48:08 +0300

    Improve Solver. Move classes to separate files

---
+++ Cyberpunk2077HackHelper.Solving/MatrixSequencePoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Cyberpunk2077HackHelper.Solving
+{
+	public class MatrixSequencePoint
+	{
+		public readonly Point Point;
+		public readonly int Index;
+		public readonly MatrixSequencePoint PrevPoint;
+
+		public MatrixSequencePoint(Point point, int index, MatrixSequencePoint prevPoint)
+		{
+			Point = point;
+			Index = index;
+			PrevPoint = prevPoint;
+		}
+	}
+}
--- Cyberpunk2077HackHelper.Solving/Solver.cs
+++ Cyberpunk2077HackHelper.Solving/Solver.cs
@@ -16,13 +16,29 @@ namespace Cyberpunk2077HackHelper.Solving
 			{
 				MatrixCellEnumerator matrixCellEnumerator = new MatrixCellEnumerator(problem.Matrix, possibleCombination);
 				MatrixCellProcessor matrixCellProcessor = new MatrixCellProcessor();
-				Wave<MatrixSequencePoint, MatrixSequencePoint> wave = new Wave<MatrixSequencePoint, MatrixSequencePoint>(matrixCellEnumerator, matrixCellProcessor);
+				Wave<MatrixSequencePoint, MatrixSequencePoint> wave = new Wave<MatrixSequencePoint, MatrixSequencePoint>(matrixCellEnumerator, matrixCellProcessor, new MatrixSequencePointComparer());
 
 				MatrixSequencePoint[] result = wave.Run().ToArray();
-
-				if (result[result.Length - 1].Index == possibleCombination.Count - 1)
-					yield return result.Select(point => point.Point).ToArray();
+				foreach (MatrixSequencePoint reusltPoint in result)
+				{
+					if (reusltPoint.Index == possibleCombination.Count - 1)
+						yield return UnwindMatrixSequencePoint(reusltPoint);
+				}
 			}
+		}
+
+		private static IReadOnlyList<Point> UnwindMatrixSequencePoint(MatrixSequencePoint matrixSequencePoint)
+		{
+			int length = matrixSequencePoint.Index + 1;
+			Point[] result = new Point[length];
+
+			for (int i = length - 1; i >= 0; --i)
+			{
+				result[i] = matrixSequencePoint.Point;
+				matrixSequencePoint = matrixSequencePoint.PrevPoint;
+			}
+
+			return result;
 		}
 
 		private IEnumerable<IReadOnlyList<Symbol>> GetPossibleSequenceCombinations(IReadOnlyList<IReadOnlyList<Symbol>> sequences, int maxCombinationLength)
