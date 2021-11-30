19832082a2081d578a6c022da856c69b429c9851
Andrey Lemin <anlemin@mail.ru>
2021-12-01 02:36:29 +0300

    Add another overlay application

---
+++ Cyberpunk2077HackHelper.Overlay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Cyberpunk2077HackHelper.Overlay
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GameOverlay.TimerService.EnableHighPrecisionTimers();
+
+			using (var example = new Example())
+			{
+				example.Run();
+			}
+		}
+	}
+}
---
+++ Cyberpunk2077HackHelper.Overlay/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Cyberpunk2077HackHelper.Overlay
+{
+	public static class Utils
+	{
+		public static IntPtr WinGetHandle(string wName)
+		{
+			foreach (Process pList in Process.GetProcesses())
+				if (pList.MainWindowTitle.Contains(wName))
+					return pList.MainWindowHandle;
+
+			return IntPtr.Zero;
+		}
+	}
+}
