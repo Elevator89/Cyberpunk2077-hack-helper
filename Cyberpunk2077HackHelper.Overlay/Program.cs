bba5f4cd5d854858865a1cda38da17a3fde93374
Andrey Lemin <anlemin@mail.ru>
2021-12-02 23:53:28 +0300

    Read layouts and symbol maps

--- Cyberpunk2077HackHelper.Overlay/Program.cs
+++ Cyberpunk2077HackHelper.Overlay/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Cyberpunk2077HackHelper.Overlay
+﻿using Cyberpunk2077HackHelper.Grabbing;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Cyberpunk2077HackHelper.Overlay
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
 			GameOverlay.TimerService.EnableHighPrecisionTimers();
+
+			string matrixSymbolMapsContents = File.ReadAllText("SymbolMaps/matrixSymbolMaps.json");
+			string sequenceSymbolMapsContents = File.ReadAllText("SymbolMaps/sequenceSymbolMaps.json");
+
+			List<SymbolMap> matrixSymbolMaps = JsonConvert.DeserializeObject<List<SymbolMap>>(matrixSymbolMapsContents);
+			List<SymbolMap> sequenceSymbolMaps = JsonConvert.DeserializeObject<List<SymbolMap>>(sequenceSymbolMapsContents);
+
+			List<Layout> layouts = new List<Layout>();
+			foreach (string layoutFileName in Directory.GetFiles("Layouts/", "Matrix*.json"))
+			{
+				string layoutContents = File.ReadAllText(layoutFileName);
+				layouts.Add(JsonConvert.DeserializeObject<Layout>(layoutContents));
+			}
 
 			using (OverlayApplication overlayApp = new OverlayApplication())
 			{
