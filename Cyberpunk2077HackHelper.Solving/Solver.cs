a9b14a2e2bad4955548de7376e6c03e79df1d489
Andrey Lemin <anlemin@mail.ru>
2021-12-05 17:43:20 +0300

    Inline Wave algo

--- Cyberpunk2077HackHelper.Solving/Solver.cs
+++ Cyberpunk2077HackHelper.Solving/Solver.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Drawing;
 using Cyberpunk2077HackHelper.Common;
-using Cyberpunk2077HackHelper.Solving.Wave;
 
 namespace Cyberpunk2077HackHelper.Solving
 {
@@ -14,31 +13,64 @@ namespace Cyberpunk2077HackHelper.Solving
 		{
 			foreach (IReadOnlyList<Symbol> possibleCombination in GetPossibleSequenceCombinations(problem.DaemonSequences, problem.BufferLength))
 			{
-				MatrixCellEnumerator matrixCellEnumerator = new MatrixCellEnumerator(problem.Matrix, possibleCombination);
-				MatrixCellProcessor matrixCellProcessor = new MatrixCellProcessor();
-				Wave<MatrixSequencePoint, MatrixSequencePoint> wave = new Wave<MatrixSequencePoint, MatrixSequencePoint>(matrixCellEnumerator, matrixCellProcessor, new MatrixSequencePointComparer());
+				List<SequenceItem> foundSequence = new List<SequenceItem>(20); // The actual size is usually less than 20
 
-				MatrixSequencePoint[] result = wave.Run().ToArray();
-				foreach (MatrixSequencePoint reusltPoint in result)
+				HashSet<Point> visitedCells = new HashSet<Point>();
+				Queue<SequenceItem> queue = new Queue<SequenceItem>();
+
+				foreach (SequenceItem initialSequenceItem in GetInitialItems())
 				{
-					if (reusltPoint.Index == possibleCombination.Count - 1)
-						yield return UnwindMatrixSequencePoint(reusltPoint);
+					visitedCells.Add(initialSequenceItem.Cell);
+					queue.Enqueue(initialSequenceItem);
+				}
+
+				while (queue.Count > 0)
+				{
+					SequenceItem currentSequenceItem = queue.Dequeue();
+
+					foundSequence.Add(currentSequenceItem);
+
+					foreach (SequenceItem nextSequenceItem in GetNextItems())
+					{
+						visitedCells.Add(nextSequenceItem.Cell);
+						queue.Enqueue(nextSequenceItem);
+					}
+
+					IEnumerable<SequenceItem> GetNextItems()
+					{
+						int nextSequenceItemIndex = currentSequenceItem.Index + 1;
+						if (nextSequenceItemIndex >= possibleCombination.Count)
+							yield break;
+
+						MatrixLineDirection nextSequenceItemLineDirection = GetSymbolDirection(nextSequenceItemIndex);
+						Symbol nextCombinationSymbol = possibleCombination[nextSequenceItemIndex];
+
+						foreach (Point nextCell in problem.Matrix.GetOtherCellsInLine(nextSequenceItemLineDirection, currentSequenceItem.Cell).Where(nextCell => !visitedCells.Contains(nextCell)))
+						{
+							if (nextCombinationSymbol == Symbol.Unknown || problem.Matrix.Get(nextCell) == nextCombinationSymbol)
+								yield return new SequenceItem(nextCell, nextSequenceItemIndex, currentSequenceItem);
+						}
+					}
+				}
+
+				foreach (SequenceItem sequenceItem in foundSequence)
+				{
+					if (sequenceItem.Index == possibleCombination.Count - 1)
+						yield return UnwindMatrixSequencePoint(sequenceItem);
+				}
+
+				IEnumerable<SequenceItem> GetInitialItems()
+				{
+					MatrixLineDirection initialSequenceItemLineDirection = GetSymbolDirection(0);
+					Symbol initialCombinationSymbol = possibleCombination[0];
+
+					foreach (Point initialCell in problem.Matrix.GetOtherCellsInLine(initialSequenceItemLineDirection, new Point(-1, 0)))
+					{
+						if (initialCombinationSymbol == Symbol.Unknown || problem.Matrix.Get(initialCell) == initialCombinationSymbol)
+							yield return new SequenceItem(initialCell, 0, null);
+					}
 				}
 			}
-		}
-
-		private static IReadOnlyList<Point> UnwindMatrixSequencePoint(MatrixSequencePoint matrixSequencePoint)
-		{
-			int length = matrixSequencePoint.Index + 1;
-			Point[] result = new Point[length];
-
-			for (int i = length - 1; i >= 0; --i)
-			{
-				result[i] = matrixSequencePoint.Point;
-				matrixSequencePoint = matrixSequencePoint.PrevPoint;
-			}
-
-			return result;
 		}
 
 		private IEnumerable<IReadOnlyList<Symbol>> GetPossibleSequenceCombinations(IReadOnlyList<IReadOnlyList<Symbol>> sequences, int maxCombinationLength)
@@ -56,5 +88,25 @@ namespace Cyberpunk2077HackHelper.Solving
 			} while (orderIsValid);
 		}
 
+
+		private static MatrixLineDirection GetSymbolDirection(int symbolIndex)
+		{
+			// 0-th symbol should be found in row, 1-th - in column, etc.
+			return symbolIndex % 2 == 0 ? MatrixLineDirection.Row : MatrixLineDirection.Column;
+		}
+
+		private static IReadOnlyList<Point> UnwindMatrixSequencePoint(SequenceItem matrixSequencePoint)
+		{
+			int length = matrixSequencePoint.Index + 1;
+			Point[] result = new Point[length];
+
+			for (int i = length - 1; i >= 0; --i)
+			{
+				result[i] = matrixSequencePoint.Cell;
+				matrixSequencePoint = matrixSequencePoint.PrevPoint;
+			}
+
+			return result;
+		}
 	}
 }
