e5169969aace27c057fc3f4e58f01ab1d925b67b
Andrey Lemin <anlemin@mail.ru>
2021-12-05 17:43:20 +0300

    Inline Wave algo

---
+++ Cyberpunk2077HackHelper.Solving/SequenceItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+namespace Cyberpunk2077HackHelper.Solving
+{
+	public class SequenceItem
+	{
+		public readonly Point Cell;
+		public readonly int Index;
+		public readonly SequenceItem PrevPoint;
+
+		public SequenceItem(Point point, int index, SequenceItem prevPoint)
+		{
+			Cell = point;
+			Index = index;
+			PrevPoint = prevPoint;
+		}
+	}
+}
